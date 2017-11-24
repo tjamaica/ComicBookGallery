a037608ba9be453fea36c7fe304b9ee83419ecf2
t.elmadady <t.elmadady@gmail.com>
2017-11-24 03:14:47 +0200

    Updated

--- ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,31 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookGallery.Data;
 using ComicBookGallery.Models;
 
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public ActionResult Detail()
-        {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spider-Man",
-                IssueNumber = 700,
-                DescriptionHtml =
-                    "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                        new Artist() {Name = "Script", Role = "Dan Slott"},
-                        new Artist() {Name = "Pencils", Role = "Humberto Ramos"},
-                        new Artist() {Name = "Inks", Role = "Victor Olazaba"},
-                        new Artist() {Name = "Colors", Role = "Edgar Delgado"},
-                        new Artist() {Name = "Letters", Role = "Chris Eliopoulos"}
-                }
-            };
+        private ComicBookRepository _comicBookRepository = null;
 
-            
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+        public ActionResult Detail(int? id)
+        {
+                if (id == null)
+                { 
+                    return HttpNotFound();
+                }
+            var comicBook = _comicBookRepository.GetComicBook(id.Value);
             return View(comicBook);
         }
     }
---
+++ ComicBookGallery/ComicBookGallery/Data/ComicBookRepository.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Data
+{
+    public class ComicBookRepository
+    {
+        private static ComicBook[] _comicBooks = new ComicBook[]
+        {
+            new ComicBook()
+            {
+                Id = 1,
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 700,
+                DescriptionHtml =
+                    "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Script", Role = "Dan Slott"},
+                    new Artist() {Name = "Pencils", Role = "Humberto Ramos"},
+                    new Artist() {Name = "Inks", Role = "Victor Olazaba"},
+                    new Artist() {Name = "Colors", Role = "Edgar Delgado"},
+                    new Artist() {Name = "Letters", Role = "Chris Eliopoulos"}
+                }
+            },
+            new ComicBook()
+            {
+                Id = 2,
+                SeriesTitle = "The Amazing Spider-Man",
+                IssueNumber = 657,
+                DescriptionHtml =
+                    "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Script", Role = "Dan Slott"},
+                    new Artist() {Name = "Pencils", Role = "Humberto Ramos"},
+                    new Artist() {Name = "Inks", Role = "Victor Olazaba"},
+                    new Artist() {Name = "Colors", Role = "Edgar Delgado"},
+                    new Artist() {Name = "Letters", Role = "Chris Eliopoulos"}
+                }
+            },
+            new ComicBook()
+            {
+                Id = 3,
+                SeriesTitle = "Bone",
+                IssueNumber = 50,
+                DescriptionHtml =
+                    "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Script", Role = "Dan Slott"},
+                    new Artist() {Name = "Pencils", Role = "Humberto Ramos"},
+                    new Artist() {Name = "Inks", Role = "Victor Olazaba"},
+                    new Artist() {Name = "Colors", Role = "Edgar Delgado"},
+                    new Artist() {Name = "Letters", Role = "Chris Eliopoulos"}
+                }
+            }
+        };
+        public ComicBook GetComicBook(int id)
+        {
+            ComicBook comicBookToReturn = null;
+            foreach (var comicBook in _comicBooks)
+            {
+                if (comicBook.Id == id)
+                {
+                    comicBookToReturn = comicBook;
+                    break;
+                }
+            }
+            return comicBookToReturn;
+        }
+    }
+}
